eb42d84a0af7fa9bd0d43ddce3f5c26da61af210
Thomas Auttenberg <senior.auttenberg@gmail.com>
2024-03-15 00:43:37 +0700

    AllDone

--- IndexedList.h
+++ IndexedList.h
@@ -2,6 +2,7 @@
 #include <compare>
 #include <stdexcept>
 #include <sstream>
+#include <stack>
 
 
 template <class T>
@@ -32,7 +33,8 @@ private:
 	* link: |          -----------------
 	*/                             
 
-	Node* storage; // exponentially growing Node* array.
+	Node* storage; // linear (+=storageCapacity) growing Node* array.
+	std::stack<size_t> storageIndexes;
 	size_t storageCapacity = INIT_LIST_SIZE;
 	size_t realSize = 0;
 
@@ -47,33 +49,34 @@ public:
 	private:
 		friend class List;
 		size_t targetIndex;
-		const_iterator(size_t targetIndex);
+		const_iterator(const List<T>* list, size_t targetIndex);
+		const List<T>* associatedContainer = nullptr;
 	public:
-		const_iterator(iterator);
+		const_iterator();
+		const_iterator(const iterator&);
+		const_iterator(const const_iterator& other);
 		T operator*() const;
-		const_iterator& operator++(int);
-		const_iterator& operator--(int);
+		const_iterator operator++(int);
+		const_iterator operator--(int);
 		const_iterator& operator++();
 		const_iterator& operator--();
-		bool operator==(const const_iterator& other);
-		const_iterator& operator=(const const_iterator& other);
+		bool operator==(const const_iterator& other) const;
 	};
 
 	class iterator {
 	private:
 		friend class List<T>;
 		size_t targetIndex;
-		iterator(size_t targetIndex);
-		List<T>& associatedContainer;
+		iterator(const List<T>*list, size_t targetIndex);
+		const List<T>* associatedContainer = nullptr;
 	public:
 		iterator();
 		T& operator*() const;
-		iterator& operator++(int);
-		iterator& operator--(int);
+		iterator operator++(int);
+		iterator operator--(int);
 		iterator& operator++();
 		iterator& operator--();
 		bool operator==(const iterator& other) const;
-		iterator& operator=(const iterator& other); // index copying
 	};
 	//using Iterator = IndexedList<T>::iterator;
 // ============ Methods ==================
@@ -90,9 +93,13 @@ public:
 	// Informational:
 	size_t size() const;
 	bool isEmpty() const;
-	//bool contains(T& value);
-	int getIndexOf(T& value) const;
+	bool contains(const T& value) const;
+	int getIndexOf(const T& value) const;
 	// Access:
+	T& front();
+	T front() const;
+	T& back();
+	T back() const;
 	T& at(size_t index);
 	T at(size_t index) const;
 	iterator begin();
@@ -101,13 +108,15 @@ public:
 	const_iterator cend() const;
 	// Modifying:
 	void clear();
-	void push_back(T& val);
-	void push_front(T& val);
-	void insert(const_iterator, T& val);
-	void insertAt(size_t index);
+	void push_back(const T& val);
+	void push_front(const T& val);
+	void pop_front();
+	void pop_back();
+	void insert(const_iterator, const T& val);
+	void insertAt(size_t index, const T& val);
 	void erase(const_iterator);
 	void erase(const_iterator from, const_iterator to);
-	void removeValue(T& value);
+	void removeValue(const T& value);
 	void removeAt(size_t index);
 
   // ================================
@@ -128,6 +137,9 @@ List<T>::List() {
 	endNode.nextNodeIndex = -1;
 	endNode.previousNodeIndex = -1;
 	storage[0] = endNode;
+	for (int i = 1; i < storageCapacity; i++) {
+		storageIndexes.push(i);
+	}
 }
 
 template<class T>
@@ -135,14 +147,27 @@ inline List<T>::List(List<T>& other)
 {
 	this->storage = new Node[other.storageCapacity];
 	this->storageCapacity = other.storageCapacity;
+	this->storageIndexes = other.storageIndexes;
+	this->head_index = other.head_index;
+	this->tail_index = other.tail_index;
 	memcpy(storage, other.storage, sizeof(Node) * storageCapacity);
+	this->realSize = other.realSize;
 }
 template<class T>
 inline List<T>::List(List<T>&& other) {
 	this->storage = other.storage;
 	this->storageCapacity = other.storageCapacity;
+	this->storageIndexes = std::move(other.storageIndexes);
+	this->head_index = other.head_index;
+	this->tail_index = other.tail_index;
 	other.storage = nullptr;
 	other.storageCapacity = 0;
+	this->realSize = other.realSize;
+}
+
+template<class T>
+inline List<T>::~List() {
+	delete[] storage;
 }
 
 template<class T>
@@ -152,7 +177,11 @@ inline List<T>& List<T>::operator=(List<T>& other)
 		delete[] storage;
 		this->storage = new Node[other.storageCapacity];
 		this->storageCapacity = other.storageCapacity;
+		this->storageIndexes = other.storageIndexes;
 		memcpy(storage, other.storage, sizeof(Node) * storageCapacity);
+		this->head_index = other.head_index;
+		this->tail_index = other.tail_index;
+		this->realSize = other.realSize;
 	}
 	return *this;
 }
@@ -164,8 +193,12 @@ inline List<T>& List<T>::operator=(List<T>&& other)
 		delete[] storage;
 		this->storage = other.storage;
 		this->storageCapacity = other.storageCapacity;
+		this->storageIndexes = std::move(other.storageIndexes);
 		other.storage = nullptr;
 		other.storageCapacity = 0;
+		this->head_index = other.head_index;
+		this->tail_index = other.tail_index;
+		this->realSize = other.realSize;
 	}
 	return *this;
 }
@@ -183,7 +216,13 @@ inline bool List<T>::isEmpty() const
 }
 
 template<class T>
-inline int List<T>::getIndexOf(T& value) const
+inline bool List<T>::contains(const T& val) const
+{
+	return getIndexOf(val) >= 0;
+}
+
+template<class T>
+inline int List<T>::getIndexOf(const T& value) const
 {
 	const_iterator it = cbegin();
 	const_iterator end = this->cend();
@@ -196,16 +235,40 @@ inline int List<T>::getIndexOf(T& value) const
 }
 
 template<class T>
+inline T& List<T>::front()
+{
+	if (size() == 0) throw std::exception("front call on empty list");
+	return *begin();
+}
+template<class T>
+inline T List<T>::front() const
+{
+	if (size() == 0) throw std::exception("front call on empty list");
+	return *cbegin();
+}
+template<class T>
+inline T& List<T>::back()
+{
+	if (size() == 0) throw std::exception("back call on empty list");
+	return *end();
+}
+template<class T>
+inline T List<T>::back() const
+{
+	if (size() == 0) throw std::exception("back call on empty list");
+	return *cend();
+}
+template<class T>
 inline T& List<T>::at(size_t index)
 {
-	if (index > realSize || index < 0) {
+	if (index > realSize-1 || index < 0) {
 		std::ostringstream message;
 		message << "List operation at: out of range\n[list size : " << realSize << " | index got : " << index << "]";
 		throw new std::out_of_range(message.str());
 	}
 
 	iterator it = begin();
-	for (int i = 0; i <= index; i++) {
+	for (int i = 0; i < index; i++) {
 		it++;
 	}
 	return *it;
@@ -220,51 +283,66 @@ inline T List<T>::at(size_t index) const
 template<class T>
 inline List<T>::iterator List<T>::begin()
 {
-	return iterator(head_index);
+	return iterator(this,head_index);
 }
 
 template<class T>
 inline List<T>::iterator List<T>::end()
 {
-	return iterator(tail_index);
+	return iterator(this,tail_index);
 }
 
 template<class T>
 inline List<T>::const_iterator List<T>::cbegin() const
 {
-	return const_iterator(head_index);
+	return const_iterator(this,head_index);
 }
 
 template<class T>
 inline List<T>::const_iterator List<T>::cend() const
 {
-	return const_iterator(tail_index);
+	return const_iterator(this,tail_index);
 }
 
 template<class T>
 inline void List<T>::clear()
 {
-	delete[] storage;
-	storage = new Node[INIT_LIST_SIZE];
-	storageCapacity = INIT_LIST_SIZE;
-	realSize = 0;
 
+	this->realSize = 0;
+	this->head_index = 0;
+	this->tail_index = 0;
+	this->storageIndexes = std::move(std::stack<size_t>());
+	for (int i = 1; i < storageCapacity; i++) {
+		storageIndexes.push(i);
+	}
+	
 }
 template<class T>
-inline void List<T>::push_back(T& val) {
+inline void List<T>::push_back(const T& val) {
 	insert(end(), val);
 }
 template<class T>
-inline void List<T>::push_front(T& val) {
+inline void List<T>::push_front(const T& val) {
 	insert(begin(), val);
 }
 template<class T>
-inline void List<T>::insert(const_iterator where, T& val) {
-	
+inline void List<T>::pop_front() {
+	if (size() == 0) throw std::exception("pop_front call on empty list");
+	erase(begin());
+}
+template<class T>
+inline void List<T>::pop_back() {
+	if (size() == 0) throw std::exception("pop_back call on empty list");
+	iterator it = --end();
+	erase(it);
+}
+template<class T>
+inline void List<T>::insert(const_iterator where, const T& val) {
+	if (where.associatedContainer != this) throw new std::invalid_argument("Insert call using iterator isn't linked to that container");
 	const_iterator tempIter = where;
 	const_iterator nextNodeIter = where;
 	const_iterator prevNodeIter;
-	if (where != begin()) prevNodeIter = (tempIter--).targetIndex;
+	if (where != begin()) prevNodeIter = (--tempIter);
 	Node* prevNode = where == begin() ? nullptr : storage + ((prevNodeIter).targetIndex);
 	Node* nextNode = storage + ((nextNodeIter).targetIndex);
 	
@@ -274,33 +352,228 @@ inline void List<T>::insert(const_iterator where, T& val) {
 	newNode.nextNodeIndex = nextNodeIter.targetIndex;
 	//newNode.previousNodeIndex = prevNode == nullptr ? -1 : prevNodeIter.targetIndex;
 	size_t newNodeIndex = pushInContainer(newNode);
+	
 	if (prevNode == nullptr) {
-		newNode.previousNodeIndex = -1;
+		storage[newNodeIndex].previousNodeIndex = -1;
 		head_index = newNodeIndex;
 	}
 	else {
-		newNode.previousNodeIndex = prevNodeIter.
+		prevNode->nextNodeIndex = newNodeIndex;
+		storage[newNodeIndex].previousNodeIndex = prevNodeIter.targetIndex;
 	}
-	prevNode->nextNodeIndex = newNodeIndex;
 	nextNode->previousNodeIndex = newNodeIndex;
 }
 template<class T>
+inline void List<T>::insertAt(size_t index, const T& val)
+{
+	iterator it = begin();
+	for (int i = 0; i < index; i++) {
+		it++;
+	}
+	insert(it, val);
+}
+template<class T>
+inline void List<T>::erase(const_iterator it)
+{
+	if (it == end())
+		throw std::invalid_argument("erase call using iterator pointing to the end of list");
+	const_iterator a = it;
+	a++;
+	erase(it, a);
+}
+template<class T>
+inline void List<T>::erase(const_iterator from, const_iterator to)
+{
+	if (from.associatedContainer != this || to.associatedContainer != this) throw new std::invalid_argument("erase call using iterator isn't linked to that container");
+	if (from == to) return;
+	bool isFromBegin = from == begin();
+	const_iterator fromPrevious;
+	if (!isFromBegin) { fromPrevious = --from; from++; };
+	do {
+		
+		try {
+			const_iterator tempIter = from++;
+			storageIndexes.push(tempIter.targetIndex);
+			Node* node = storage + tempIter.targetIndex;
+			node->nextNodeIndex = -1;
+			node->previousNodeIndex = -1;
+			realSize--;
+		}
+		catch (std::logic_error) {
+			throw new std::invalid_argument("erase call using invalid \"from\" iterator");
+		}
 
+	} while (from != to && from != end());
+
+	if (!isFromBegin) {
+		storage[to.targetIndex].previousNodeIndex = fromPrevious.targetIndex;
+		storage[fromPrevious.targetIndex].nextNodeIndex = to.targetIndex;
+	}
+	else {
+		head_index = to.targetIndex;
+	}
+}
+template<class T>
+inline void List<T>::removeValue(const T& value)
+{
+	const_iterator it = begin();
+	
+	while (it != end()) {
+		const_iterator tempIter = it++;
+		if (*tempIter == value)
+			erase(tempIter);
+	}
+
+}
+template<class T>
+inline void List<T>::removeAt(size_t index)
+{
+	if (index >= realSize) throw new std::out_of_range("Out of range exception on calling removeAt");
+	const_iterator it = begin();
+	for (int i = 0; i <= index; i++, it++);
+	erase(it);
+}
+template<class T>
 inline size_t List<T>::pushInContainer(Node& val) {
 	
-	if (realSize-1 == storageCapacity) {
+	if (realSize == storageCapacity-2) { // 1 element is reserved to endNode
 		Node* storage_ = new Node[storageCapacity + INIT_LIST_SIZE];
 		memcpy(storage_, storage, sizeof(Node) * storageCapacity);
+		for (int i = storageCapacity; i < storageCapacity + INIT_LIST_SIZE; i++) {
+			storageIndexes.push(i);
+		}
 		storageCapacity += INIT_LIST_SIZE;
 		delete[] storage;
 		storage = storage_;
 	}
-	storage[++realSize] = val;
-	return realSize;
+	size_t newElemIndex = storageIndexes.top();
+	storageIndexes.pop();
+	
+	storage[newElemIndex] = val;
+	realSize++;
+	return newElemIndex;
 }
 
+/*
 
+		Iterators
 
+*/
 
+// const iterator
 
+template<class T>
+inline List<T>::const_iterator::const_iterator() {}
 
+template<class T>
+inline List<T>::const_iterator::const_iterator(const List<T>* list, size_t targetIndex) {
+	this->targetIndex = targetIndex;
+	this->associatedContainer = list;
+}
+
+template<class T>
+inline List<T>::const_iterator::const_iterator(const List<T>::iterator& other) {
+	this->targetIndex = other.targetIndex;
+	this->associatedContainer = other.associatedContainer;
+}
+template<class T>
+inline List<T>::const_iterator::const_iterator(const List<T>::const_iterator& other) {
+	this->targetIndex = other.targetIndex;
+	this->associatedContainer = other.associatedContainer;
+}
+
+template<class T>
+inline T List<T>::const_iterator::operator*() const {
+	if (*this == associatedContainer->cend()) throw std::logic_error("operator*: Can't get the value of the end element");
+	if (this->associatedContainer == nullptr) throw std::logic_error("operator*: Iterator isn't linked to any container");
+	return associatedContainer->storage[targetIndex].value;
+}
+template<class T>
+inline List<T>::const_iterator& List<T>::const_iterator::operator++(){
+	if (*this == associatedContainer->cend()) throw std::logic_error("operator++: Can't increment the iterator pointing at the end element");
+	if (this->associatedContainer == nullptr) throw std::logic_error("operator++: Iterator isn't linked to any container");
+	targetIndex = associatedContainer->storage[targetIndex].nextNodeIndex;
+	return *this;
+}
+template<class T>
+inline List<T>::const_iterator List<T>::const_iterator::operator++(int) {
+	if (*this == associatedContainer->cend()) throw std::logic_error("operator++: Can't increment the iterator pointing at the end element");
+	if (this->associatedContainer == nullptr) throw std::logic_error("operator++: Iterator isn't linked to any container");
+	const_iterator tempIterator = *this;
+	++(*this);
+	return tempIterator;
+}
+template<class T>
+inline List<T>::const_iterator& List<T>::const_iterator::operator--(){
+	if (*this == associatedContainer->cbegin()) throw std::logic_error("operator--: Can't decriment the iterator pointing at the begin element");
+	if (this->associatedContainer == nullptr) throw std::logic_error("operator--: Iterator isn't linked to any container");
+	targetIndex = associatedContainer->storage[targetIndex].previousNodeIndex;
+	return *this;
+}
+template<class T>
+inline List<T>::const_iterator List<T>::const_iterator::operator--(int) {
+	if (*this == associatedContainer->cbegin()) throw std::logic_error("operator--: Can't decriment the iterator pointing at the begin element");
+	if (this->associatedContainer == nullptr) throw std::logic_error("operator--: Iterator isn't linked to any container");
+	const_iterator tempIterator = *this;
+	--(*this);
+	return tempIterator;
+}
+template<class T>
+inline bool List<T>::const_iterator::operator==(const List<T>::const_iterator& other) const {
+	if (other.associatedContainer == this->associatedContainer && this->targetIndex == other.targetIndex) 
+		return true;
+	return false;
+}
+
+// non const iterator
+
+template<class T>
+inline List<T>::iterator::iterator(){}
+
+template<class T>
+inline List<T>::iterator::iterator(const List<T>* list, size_t targetIndex) {
+	associatedContainer = list;
+	this->targetIndex = targetIndex;
+}
+template<class T>
+inline T& List<T>::iterator::operator*() const {
+	if (*this == associatedContainer->cend()) throw std::logic_error("operator*: Can't get the value of the end element");
+	if (this->associatedContainer == nullptr) throw std::logic_error("operator*: Iterator isn't linked to any container");
+	return (associatedContainer->storage[targetIndex].value);
+}
+template<class T>
+inline List<T>::iterator& List<T>::iterator::operator++() {
+	if (*this == associatedContainer->cend()) throw std::logic_error("operator++: Can't increment the iterator pointing at the end element");
+	if (this->associatedContainer == nullptr) throw std::logic_error("operator++: Iterator isn't linked to any container");
+	targetIndex = associatedContainer->storage[targetIndex].nextNodeIndex;
+	return *this;
+}
+template<class T>
+inline List<T>::iterator List<T>::iterator::operator++(int) {
+	if (*this == associatedContainer->cend()) throw std::logic_error("operator++: Can't increment the iterator pointing at the end element");
+	if (this->associatedContainer == nullptr) throw std::logic_error("operator++: Iterator isn't linked to any container");
+	iterator tempIterator = *this;
+	++(*this);
+	return tempIterator;
+}
+template<class T>
+inline List<T>::iterator& List<T>::iterator::operator--() {
+	if (*this == associatedContainer->cbegin()) throw std::logic_error("operator--: Can't decriment the iterator pointing at the begin element");
+	if (this->associatedContainer == nullptr) throw std::logic_error("operator--: Iterator isn't linked to any container");
+	targetIndex = associatedContainer->storage[targetIndex].previousNodeIndex;
+	return *this;
+}
+template<class T>
+inline List<T>::iterator List<T>::iterator::operator--(int) {\
+	if (*this == associatedContainer->cbegin()) throw std::logic_error("operator--: Can't decriment the iterator pointing at the begin element");
+	if (this->associatedContainer == nullptr) throw std::logic_error("operator--: Iterator isn't linked to any container");
+	iterator tempIterator = *this;
+	--(*this);
+	return tempIterator;
+}
+template<class T>
+inline bool List<T>::iterator::operator==(const List<T>::iterator& other) const {
+	if (other.associatedContainer == this->associatedContainer && this->targetIndex == other.targetIndex)
+		return true;
+	return false;
+}
--- List.cpp
+++ List.cpp
@@ -7,17 +7,71 @@
 #include <list>
 int main()
 {
-    int i;
-    std::cout << "Hello World!\n";
-    List<int>::iterator;
-    const std::list<int> a_;
-    a_.begin()++;
-   
-    //std::string k = "hello" + 12 + " world";
-    std::list<int> a(a_);
-    //auto it = a.begin();
-    //it++; it--; *it;
+    List<int> a;
+    a.push_back(0);
+    a.size();
+    a.push_back(1);
+    a.push_back(2);
+    a.push_back(3);
+    a.push_front(4);
+    
+    List<int>::iterator b = a.begin();
+    b++;
+    List<int>::iterator c = b;
+    c++;
+    c++;
+    a.clear();
+    //std::cout << *b << std::endl;
+    //a.erase(a.begin(), a.end());
+    try {
+        a.push_back(0);
+        a.push_front(1);
+        a.push_back(2);
+        a.push_back(3);
+        a.push_back(4);
+        a.push_back(4);
+        a.push_back(4);
+        auto it = a.begin();
+        it++;
+        it++;
+        it++;
+        it++;
+        it++;
+        a.insert(it, 15);
+        a.insertAt(3, 25);
+        a.front() = -1;
+        a.removeValue(4);
+        //a.pop_front();
+        std::cout << "index of 25:" << a.getIndexOf(25)<<"\n";
+        std::cout << a.at(3) <<" " << a.size() << "\n";
+    }
+    catch (std::logic_error e) {
+        std::cout << e.what() << "\n";
+    }
+    //std::list<int> s;
+   // s.push_back(0);
+   // s.push_back(1);
+   // s.push_back(2);
+    //s.push_back(3);
+   // s.push_front(4);
+   //std::list<int>::iterator iter = s.begin();
+    //iter++;
+    //std::list<int>::iterator iter_ = iter;
+    //++iter_;
+    //s.erase(iter, ++++iter);
+    //for (std::list<int>::iterator i = s.begin(); i != s.end(); i++) {
+   //     std::cout << *i << "\n";
+    //}
 
+    try {
+        
+        for (List<int>::iterator it = a.begin(); it != a.end(); it++) {
+            std::cout << " " << *it;
+        }
+    }
+    catch (std::logic_error e) {
+        std::cout << e.what();
+    }
 }
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
